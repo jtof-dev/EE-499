20b2da1e4ce6b6b1a732ecfba9b1782c3293a4ec
Andy Babcock <andybabcock@jtof.dev>
2026-04-09 17:50:46 -0700

    small update

--- src/wearable-data-analysis/main.py
+++ src/wearable-data-analysis/main.py
@@ -1,23 +1,6 @@
 import pandas as pd
 
-# Load the dataset
+# load the dataset
 dataset = pd.read_csv("sample_eeg_data.csv")
-
-# Now you can easily isolate your variables for machine learning:
-X = dataset[
-    [
-        "Delta",
-        "Theta",
-        "Alpha1",
-        "Alpha2",
-        "Beta1",
-        "Beta2",
-        "Gamma1",
-        "Gamma2",
-        "Attention",
-        "Meditation",
-    ]
-]
-y = dataset["label"]
 
 print(dataset["Alpha2"].head(10))
